c7234ed57beed9fcc8295e39b0d85af9fdc469d7
Ian Perfitt <ianperfitt@gmail.com>
2020-02-24 14:29:49 -0600

    Working on displaying the maps for the More Info component

--- client-side/src/app/app.component.ts
+++ client-side/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+import { CookieService } from 'ngx-cookie-service'; 
 
 @Component({
   selector: 'app-root',
@@ -12,7 +12,18 @@ export class AppComponent implements OnInit {
 
   private title = "DIY Event App"
 
-  constructor(private cookieService: CookieService) {}
+  constructor(private cookieService: CookieService) {
+    // this.oauthService.redirectUri = window.location.origin;
+    // this.oauthService.clientId = '{client-id}';
+    // this.oauthService.scope = 'openid profile email';
+    // this.oauthService.issuer = 'https://dev-{dev-id}.oktapreview.com';
+    // this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+
+    // // Load Discovery Document and then try to login the user
+    // this.oauthService.loadDiscoveryDocument().then(() => {
+    //   this.oauthService.tryLogin();
+    // });
+  }
 
   public ngOnInit(): void {
     this.cookieService.set('cookie-name', 'our cookie value');
