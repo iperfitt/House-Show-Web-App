185751dcbfc95da7701f4dd2eb8e82d735e82ff6
Ian Perfitt <ianperfitt@gmail.com>
2020-02-26 16:36:00 -0600

    added correct annotations to the UserServiceImpl

--- server-side/src/main/java/com/ianperfitt/Repo/UserRepo.java
+++ server-side/src/main/java/com/ianperfitt/Repo/UserRepo.java
@@ -1,9 +1,15 @@
 package com.ianperfitt.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ianperfitt.Entities.User;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
+
+	@Transactional
+	User findByEmailAddress(User u);
 
 }
--- server-side/src/main/java/com/ianperfitt/Services/UserServiceImpl.java
+++ server-side/src/main/java/com/ianperfitt/Services/UserServiceImpl.java
@@ -1,5 +1,20 @@
 package com.ianperfitt.Services;
 
-public class UserServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ianperfitt.Entities.User;
+import com.ianperfitt.Repo.UserRepo;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepo ur;
+
+	public void register(User u) {
+		System.out.println(ur.findByEmailAddress(u));
+
+	}
 
 }
