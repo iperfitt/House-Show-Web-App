726f2aa6a054578abe3ccb850d2633ceafd8dd61
Ian Perfitt <ianperfitt@gmail.com>
2020-02-28 13:04:06 -0600

    removed login and registration functionality. will add in in later

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -5,16 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { EventListComponent } from './components/event-list/event-list.component';
 import { AppRoutingModule } from './app-routing.module';
-import { EventCreationFormComponent } from './components/forms/event-creation-form/event-creation-form.component';
+import { EventCreationFormComponent } from 'src/app/components/forms/forms/event-creation/event-creation-form.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
 import { AgmCoreModule } from '@agm/core';
 import { EventInfoComponent } from './components/event-info/event-info.component';
 import { CookieService } from 'ngx-cookie-service';
-import { LoginComponent } from './components/login/login.component';
-import { LoginFormComponent } from './components/forms/login-form/login-form.component';
 import { EventService } from './services/event/event.service';
-import { RegisterComponent } from './components/register/register.component';
-import { RegisterFormComponent } from './components/forms/register-form/register-form.component';
 
 
 @NgModule({
@@ -23,11 +19,7 @@ import { RegisterFormComponent } from './components/forms/register-form/register
     EventListComponent,
     CreateEventComponent,
     EventCreationFormComponent,
-    EventInfoComponent,
-    LoginComponent,
-    LoginFormComponent,
-    RegisterComponent,
-    RegisterFormComponent
+    EventInfoComponent
   ],
   imports: [
     BrowserModule,
